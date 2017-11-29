86b88fb624fa73a030585ccdcdcc1dc981683978
Tyler Ferrara <tylerferrara123@gmail.com>
2017-11-29 11:57:04 -0500

    non-terminal class and all subclasses

---
+++ ExpressionEditor/src/NonTerminal.java
@@ -0,0 +1,40 @@
+
+public class NonTerminal implements CompoundExpression {
+
+	@Override
+	public CompoundExpression getParent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setParent(CompoundExpression parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Expression deepCopy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void flatten() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String convertToString(int indentLevel) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addSubexpression(Expression subexpression) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ ExpressionEditor/src/Parentheses.java
@@ -0,0 +1,4 @@
+
+public class Parentheses extends NonTerminal {
+
+}
