996208f8d103f41bcdda76fe2ff8315a56dd64e0
Tyler Ferrara <tylerferrara123@gmail.com>
2017-12-13 15:22:55 -0500

    d

--- Gui/src/Multiply.java
+++ Gui/src/Multiply.java
@@ -3,7 +3,7 @@ import javafx.scene.control.Label;
 
 public class Multiply extends NonTerminal {
 	public Multiply() {
-		
+		super.pane.getChildren().add(new Label("*"));
 	}
 	public String toString() {
 		return "*";
